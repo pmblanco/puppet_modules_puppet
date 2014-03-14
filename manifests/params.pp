986c327e5c4a5543cf60a65e19d2c5cdb1b279ff
Pedro Miguel Blanco Gómez <pmblanco@ardemans.com>
2014-03-14 07:37:43 +0100

    Problemas añadiendo repos con modulo apt.

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,6 +4,7 @@ class puppet::params {
   
     'Ubuntu': {
 	  $puppet_source_repo_url   = 'http://apt.puppetlabs.com'
+	  $puppet_source_repo_key   = '4BD6EC30'
 	  $puppet_source_repos      = 'main dependencies'
 	  $puppet_master_package    = 'puppetmaster'
 	  $puppet_agent_package     = 'puppet'
--- manifests/repo/ubuntu.pp
+++ manifests/repo/ubuntu.pp
@@ -8,11 +8,18 @@ class puppet::repo::ubuntu {
       fail ("Version $::operatingsystemrelease of Ubuntu is not supported")
     }
   }
+  
+  apt::key { 'puppetlabs':
+    key        => '4BD6EC30',
+    key_server => 'pgp.mit.edu',
+  }
 
   apt::source { 'puppetlabs-repo':
-    location     => "$puppet_source_repo_url",
+    location     => "$puppet::params::puppet_source_repo_url",
     release      => "$ubuntu_release",
-    repos        => "$puppet_source_repos",
+    repos        => "$puppet::params::puppet_source_repos",
+	key          => "$puppet::params::puppet_source_repo_key",
+	key_server   => "pgp.mit.edu",
   }
 
 }
