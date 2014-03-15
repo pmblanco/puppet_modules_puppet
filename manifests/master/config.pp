c5ae5a2196921e74addc71ed583f6625c72e235b
Pedro Miguel Blanco Gómez <pmblanco@ardemans.com>
2014-03-15 23:12:02 +0100

    Se crea un nuevo manifiesto de creacion del concat de /etc/puppet/puppet.conf y se hace include desde los dos puntos de configuracion del fichero... master y agent

--- manifests/master/config.pp
+++ manifests/master/config.pp
@@ -5,16 +5,8 @@ class puppet::master::config (
   $reports        = undef,
   $dns_alt_names  = undef,
 ){
-
   
-  if ! defined(Concat['/etc/puppet/puppet.conf']) {
-    concat { '/etc/puppet/puppet.conf':
-      owner   => 'root',
-	  group   => 'root',
-	  mode    => '0644',
-	  require => Package[$puppet::params::puppet_master_package],
-    }
-  }
+  include puppet::puppetconf
   
   concat::fragment { 'puppet_conf_master':
     target  => '/etc/puppet/puppet.conf',
