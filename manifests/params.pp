b801c350a3323630d2addc18be60e39fc30d7ff7
Pedro Miguel Blanco Gómez <pmblanco@ardemans.com>
2015-04-28 15:37:27 +0200

    Añadimos compatibilidad con Debian

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,7 +2,7 @@ class puppet::params {
 
   case $::operatingsystem {
 
-    'Ubuntu': {
+    'Ubuntu','Debian': {
 	  $puppet_source_repo_url        = 'http://apt.puppetlabs.com'
 	  $puppet_source_repo_key        = '4BD6EC30'
 	  $puppet_source_repo_key_server = 'pgp.mit.edu'
